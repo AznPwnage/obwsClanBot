dc045d105ba1c0c7e3ab43d72c9e4c9fe5039271
AznPwnage <dnd.tordek@gmail.com>
2021-02-09 03:15:35 -0800

    Completed all milestone checks with score logic

--- main.py
+++ main.py
@@ -18,13 +18,35 @@ class DestinyRaid(enum.Enum):
     dsc = 910380154
     lw = 2122313384
 
+
 MIN_LIGHT = 1200
 
 CLAN_ENGRAM_MS_HASH = '3603098564'
 CLAN_ENGRAM_OBJ_HASH = 1001409310
 CRUCIBLE_MS_HASH = '2594202463'
 CRUCIBLE_OBJ_HASH = 4026431786
-EXO_CHALLENGE_MS_HASH = 979073379
+EXO_CHALLENGE_MS_HASH = '979073379'
+BANSHEE_ENGRAM_MS_HASH = '3899487295'
+BANSHEE_ENGRAM_OBJ_HASH = 313458118
+DRIFTER_ENGRAM_MS_HASH = '3802603984'
+DRIFTER_ENGRAM_OBJ_HASH = 967175154
+ZAVALA_ENGRAM_MS_HASH = '2709491520'
+ZAVALA_ENGRAM_OBJ_HASH = 2338381314
+VARIKS_ENGRAM_MS_HASH = '2540726600'
+VARIKS_ENGRAM_OBJ_HASH = 1619867321
+EXO_STRANGER_MS_HASH = '1424672028'
+EXO_STRANGER_OBJ_HASH = 340542773
+EMPIRE_HUNT_MS_HASH = '291895718'
+NIGHTFALL_MS_HASH = '1942283261'
+DEADLY_VENATICS_MS_HASH = '2406589846'
+STRIKES_MS_HASH = '1437935813'
+NIGHTFALL_100K_MS_HASH = '2029743966'
+GAMBIT_MS_HASH = '3448738070'
+CRUCIBLE_PLAYLIST_MS_HASH = '3312774044'
+CRUCIBLE_GLORY_MS_HASH = '1368032265'
+TRIALS3_MS_HASH = '3628293757'
+TRIALS5_MS_HASH = '3628293755'
+TRIALS7_MS_HASH = '3628293753'
 
 EXO_CHALLENGE_HASHES = [1262994080, 2361093350, 3784931086]
 
@@ -39,6 +61,22 @@ def initialize_member(clan_member):
     member.crucible_engram = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
 
     member.exo_challenge = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.banshee = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.drifter = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.zavala = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.variks = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.exo_stranger = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.empire_hunt = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.nightfall = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.deadly_venatics = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.strikes = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.nightfall_100k = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.gambit = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.crucible_playlist = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.crucible_glory = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.trials3 = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.trials5 = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
+    member.trials7 = {DestinyClass.Hunter.name: False, DestinyClass.Warlock.name: False, DestinyClass.Titan.name: False}
 
     member.raids = {DestinyRaid.gos.name: {DestinyClass.Hunter.name: 0, DestinyClass.Warlock.name: 0, DestinyClass.Titan.name: 0},
                     DestinyRaid.dsc.name: {DestinyClass.Hunter.name: 0, DestinyClass.Warlock.name: 0, DestinyClass.Titan.name: 0},
@@ -134,18 +172,160 @@ def get_exo_challenge(member, member_class, milestones_list, activity_hash_arr):
     return member
 
 
+def get_banshee(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_collectible_milestone(milestones_list, BANSHEE_ENGRAM_MS_HASH, BANSHEE_ENGRAM_OBJ_HASH):
+            member.banshee[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_drifter(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_collectible_milestone(milestones_list, DRIFTER_ENGRAM_MS_HASH, DRIFTER_ENGRAM_OBJ_HASH):
+            member.drifter[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_zavala(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_collectible_milestone(milestones_list, ZAVALA_ENGRAM_MS_HASH, ZAVALA_ENGRAM_OBJ_HASH):
+            member.zavala[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_variks(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_collectible_milestone(milestones_list, VARIKS_ENGRAM_MS_HASH, VARIKS_ENGRAM_OBJ_HASH):
+            member.variks[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_exo_stranger(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_collectible_milestone(milestones_list, EXO_STRANGER_MS_HASH, EXO_STRANGER_OBJ_HASH):
+            member.exo_stranger[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_empire_hunt(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, EMPIRE_HUNT_MS_HASH):
+            member.exo_challenge[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_nightfall(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, NIGHTFALL_MS_HASH):
+            member.nightfall[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_deadly_venatics(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, DEADLY_VENATICS_MS_HASH):
+            member.deadly_venatics[member_class.name] = True
+            member.score += 2
+    return member
+
+
+def get_strikes(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, STRIKES_MS_HASH):
+            member.strikes[member_class.name] = True
+            member.score += 3
+    return member
+
+
+def get_nightfall_100k(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, NIGHTFALL_100K_MS_HASH):
+            member.nightfall_100k[member_class.name] = True
+            member.score += 3
+    return member
+
+
+def get_gambit(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, GAMBIT_MS_HASH):
+            member.gambit[member_class.name] = True
+            member.score += 3
+    return member
+
+
+def get_crucible_playlist(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, CRUCIBLE_PLAYLIST_MS_HASH):
+            member.crucible_playlist[member_class.name] = True
+            if 'PVP' == member.clan_type:
+                member.score += 2
+            member.score += 3
+    return member
+
+
+def get_crucible_glory(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, CRUCIBLE_GLORY_MS_HASH):
+            member.crucible_glory[member_class.name] = True
+            if 'PVP' == member.clan_type:
+                member.score += 2
+            member.score += 3
+    return member
+
+
+def get_trials(member, member_class, milestones_list):
+    if not member.low_light[member_class.name]:
+        if check_auto_milestone(milestones_list, TRIALS3_MS_HASH):
+            member.trials3[member_class.name] = True
+            if 'PVP' == member.clan_type:
+                member.score += 2
+            member.score += 2
+            if check_auto_milestone(milestones_list, TRIALS5_MS_HASH):
+                member.trials5[member_class.name] = True
+                if 'PVP' == member.clan_type:
+                    member.score += 2
+                member.score += 3
+                if check_auto_milestone(milestones_list, TRIALS7_MS_HASH):
+                    member.trials7[member_class.name] = True
+                    if 'PVP' == member.clan_type:
+                        member.score += 2
+                    member.score += 4
+    return member
+
+
+def apply_score_cap_and_decay(member):
+    if member.score > 40:
+        member.score = 40
+    if clan.clan_type == 'Regional':  # point decay for regional clan
+        member.score -= 10
+    return member
+
+
 def write_members_to_csv(mem_list):
     with open('members.csv', 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(
-            ['Name', 'Score', 'Id', 'Clan', 'MemberShipType', 'ClanType', 'GOS', 'DSC', 'LW', 'ExoChallenge',
-             'ClanEngram', 'CrucibleEngram', 'LowLight', 'PrivacyFlag', 'AccountExistsFlag'])
+            ['Name', 'Score', 'Id', 'Clan', 'MemberShipType', 'ClanType', 'GOS', 'DSC', 'LW', 'ClanEngram',
+             'CrucibleEngram', 'ExoChallenge', 'SpareParts', 'ShadySchemes', 'VanguardServices', 'ExoStranger',
+             'EmpireHunt', 'NightFall', 'DeadlyVenatics', 'Strikes', 'Nightfall100k', 'Gambit', 'CruciblePlaylist',
+             'CrucibleGlory', 'Trials3', 'Trials5', 'Trials7', 'LowLight', 'PrivacyFlag', 'AccountExistsFlag'])
         for member in mem_list:
             writer.writerow(
                 [str(member.name), str(member.score), str(member.membership_id), str(member.clan_name), str(member.membership_type),
                  str(member.clan_type), str(member.raids[DestinyRaid.gos.name]), str(member.raids[DestinyRaid.dsc.name]),
-                 str(member.raids[DestinyRaid.lw.name]), str(member.exo_challenge), str(member.clan_engram),
-                 str(member.crucible_engram), str(member.low_light), str(member.privacy), str(member.account_not_exists)])
+                 str(member.raids[DestinyRaid.lw.name]), str(member.clan_engram), str(member.crucible_engram),
+                 str(member.exo_challenge), str(member.banshee), str(member.drifter), str(member.zavala), str(member.exo_stranger),
+                 str(member.empire_hunt), str(member.nightfall), str(member.deadly_venatics), str(member.strikes),
+                 str(member.nightfall_100k), str(member.gambit), str(member.crucible_playlist), str(member.crucible_glory),
+                 str(member.trials3), str(member.trials5), str(member.trials7), str(member.low_light), str(member.privacy),
+                 str(member.account_not_exists)])
 
 
 if __name__ == '__main__':
@@ -176,9 +356,6 @@ if __name__ == '__main__':
 
             curr_member = initialize_member(clan.memberList[j])
 
-            if clan.clan_type == 'Regional':  # point decay for regional clan
-                curr_member.score -= 10
-
             profile = profile_responses[j].json()
 
             if profile['ErrorStatus'] != 'Success':  # check for account existing or not, unsure of root cause
@@ -194,7 +371,6 @@ if __name__ == '__main__':
             character_progressions = profile['Response']['characterProgressions']['data']
             character_activities = profile['Response']['characterActivities']['data']
             curr_class = None
-            print(curr_member.name)
             for character_id in character_progressions.keys():  # iterate over single member's characters
                 milestones = character_progressions[character_id]['milestones']
                 activity_hashes = build_activity_hashes(character_activities[character_id]['availableActivities'])
@@ -205,6 +381,22 @@ if __name__ == '__main__':
                 curr_member = get_clan_engram(curr_member, curr_class, milestones)
                 curr_member = get_crucible_engram(curr_member, curr_class, milestones)
                 curr_member = get_exo_challenge(curr_member, curr_class, milestones, activity_hashes)
+                curr_member = get_banshee(curr_member, curr_class, milestones)
+                curr_member = get_drifter(curr_member, curr_class, milestones)
+                curr_member = get_zavala(curr_member, curr_class, milestones)
+                curr_member = get_variks(curr_member, curr_class, milestones)
+                curr_member = get_exo_stranger(curr_member, curr_class, milestones)
+                curr_member = get_empire_hunt(curr_member, curr_class, milestones)
+                curr_member = get_nightfall(curr_member, curr_class, milestones)
+                curr_member = get_deadly_venatics(curr_member, curr_class, milestones)
+                curr_member = get_strikes(curr_member, curr_class, milestones)
+                curr_member = get_nightfall_100k(curr_member, curr_class, milestones)
+                curr_member = get_gambit(curr_member, curr_class, milestones)
+                curr_member = get_crucible_playlist(curr_member, curr_class, milestones)
+                curr_member = get_crucible_glory(curr_member, curr_class, milestones)
+                curr_member = get_trials(curr_member, curr_class, milestones)
+
+            curr_member = apply_score_cap_and_decay(curr_member)
             curr_member_list.append(curr_member)
 
     write_members_to_csv(curr_member_list)

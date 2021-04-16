4a4a919ed632e84e0694c1706ab6b37fec9b1eae
AznPwnage <dnd.tordek@gmail.com>
2021-04-15 19:32:46 -0700

    clan_view.html:   - Implemented external score   - Implemented save to csv   - Modified location copy column to copy inactives dashboard.py:   - Implemented save to csv

--- flaskr/dashboard.py
+++ flaskr/dashboard.py
@@ -1,5 +1,7 @@
 import csv
+import os
 import os.path as path
+import urllib
 from datetime import datetime
 
 from flask import (
@@ -49,6 +51,7 @@ def clan_view():
         else:
             for row in csv_reader:
                 members.append(row)
+        print(members)
 
     return render_template('dashboard/clan_view.html', members=members, clan_name=clan_name,
                            selected_date=selected_date)
@@ -127,7 +130,18 @@ def diff_view():
 
 @dashboard.route('/save')
 def save_to_csv():
-    return ''
+    m = request.args.get('m', None)
+    clan = request.args.get('clan', None)
+    date = request.args.get('date', None)
+    url = request.args.get('old_url', None).replace('%26', '&')
+
+    m = urllib.parse.unquote(m)
+
+    file_path = get_file_path(clan, date)
+
+    write_member_string_to_csv(file_path, m)
+
+    return redirect(url)
 
 
 def get_file_path(clan_name, selected_date):
@@ -135,3 +149,12 @@ def get_file_path(clan_name, selected_date):
     week_start = score_gen.get_week_start(dt)
     week_folder = f'{week_start:%Y-%m-%d}'
     return path.join(week_folder, clan_name + '.csv')
+
+
+def write_member_string_to_csv(file_path, member_data):
+    if path.exists(file_path):  # this week's data is already generated for this clan, delete it
+        os.remove(file_path)
+    with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        csvfile.write('Name,Score,ScoreDelta,PreviousScore,DaysLastPlayed,DateLastPlayed,Id,Clan,MemberShipType,ClanType,Inactive,GOS_H,GOS_W,GOS_T,DSC_H,DSC_W,DSC_T,LW_H,LW_W,LW_T,ClanEngram_H,ClanEngram_W,ClanEngram_T,CrucibleEngram_H,CrucibleEngram_W,CrucibleEngram_T,ExoChallenge_H,ExoChallenge_W,ExoChallenge_T,SpareParts_H,SpareParts_W,SpareParts_T,ShadySchemes_H,ShadySchemes_W,ShadySchemes_T,VanguardServices_H,VanguardServices_W,VanguardServices_T,Variks_H,Variks_W,Variks_T,ExoStranger_H,ExoStranger_W,ExoStranger_T,EmpireHunt_H,EmpireHunt_W,EmpireHunt_T,NightFall_H,NightFall_W,NightFall_T,DeadlyVenatics_H,DeadlyVenatics_W,DeadlyVenatics_T,Strikes_H,Strikes_W,Strikes_T,Nightfall100k_H,Nightfall100k_W,Nightfall100k_T,Gambit_H,Gambit_W,Gambit_T,CruciblePlaylist_H,CruciblePlaylist_W,CruciblePlaylist_T,CrucibleGlory_H,CrucibleGlory_W,CrucibleGlory_T,Trials3_H,Trials3_W,Trials3_T,Trials5_H,Trials5_W,Trials5_T,Trials7_H,Trials7_W,Trials7_T,LowLight_H,LowLight_W,LowLight_T,PrivacyFlag,AccountExistsFlag,ExternalScore')
+        csvfile.write('\r\n')
+        csvfile.write(member_data)
